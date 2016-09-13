8036ba03daa736d91362700e416e49d6a92de523
istommao <istommao@gmail.com>
2016-09-13 15:27:57 +0800

    Add changelog

---
+++ changelog.md
@@ -0,0 +1,3 @@
+# Version 0.1.1
+
+- Add python2.7 support
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 INSTALL_REQUIRES = [
 ]
-VERSION = '0.1.0'
+VERSION = '0.1.1'
 
 setup(
     name='pyvalidators',
